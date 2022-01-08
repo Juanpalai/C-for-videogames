5694410f62e0f0aa827638d5fddfe39c34a61e38
Juan Pablo Lagos <juan.pablo.lagosi@gmail.com>
2022-01-08 17:40:27 +0900

    Wallking
    Inteoduce a Walling controlers and a walliikng animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     //Varible player momvent
     public float jumpForce = 6f;
+    public float runningSpeed = 2f;
     Rigidbody2D rigidBody;
     Animator animator;
 
     private const string STATE_LIVE = "isAlive";
-    private const string STATE_ON_THE_GROUND = "isOnTheGround";   
+    private const string STATE_ON_THE_GROUND = "isOnTheGround";
+    private const string STATE_WALLKING = "isWallking"; 
+
+
 
 
     public LayerMask groundMask;
@@ -37,7 +41,29 @@ public class PlayerController : MonoBehaviour
 
         animator.SetBool(STATE_ON_THE_GROUND, IsTochingTheGround());
 
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            animator.SetBool(STATE_WALLKING, true);
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            animator.SetBool(STATE_WALLKING, true);
+        }
+        if (rigidBody.velocity.x == 0)
+        {
+            animator.SetBool(STATE_WALLKING, false);
+        }
+
+
         Debug.DrawRay(this.transform.position, Vector2.down * 1.4f, Color.red);
+        
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * runningSpeed, rigidBody.velocity.y);
     }
 
     void Jump()
@@ -49,7 +75,7 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    //bollean for ksnow is the player is toching the ground
+    //bollean for know is the player is toching the ground
 
     bool IsTochingTheGround()
     {
